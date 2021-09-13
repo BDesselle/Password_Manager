922519be4088454a5204e1a7da852047fe06b7aa
Brandon Desselle <desselle.brandon2@gmail.com>
2021-09-13 10:18:23 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,27 @@
-const express = require('express')
-const app = express()
-const PORT = 3001
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+require('dotenv').config();
+
+// PROCESS.ENV
+const PORT = process.env.PORT;
+const USER = process.env.USER;
+const HOST = process.env.HOST;
+const PASSWORD = process.env.PASSWORD;
+const DATABASE = process.env.DATABASE;
+// PROCESS.ENV
+
+const db = mysql.createConnection({
+    user: USER,
+    host: HOST,
+    password: PASSWORD,
+    database: DATABASE
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
 
 app.listen(PORT, ()=>{
-    console.log('Server is running...')
+    console.log(`Server is listening ${HOST}:${PORT}`);
 });
