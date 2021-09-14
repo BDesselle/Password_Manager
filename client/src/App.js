6359d5bf7da04e0ba46c8fec3472d1c58746c5d7
Brandon Desselle <desselle.brandon2@gmail.com>
2021-09-14 08:52:21 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,38 @@
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [password, setPassword] = useState("");
+
+  const addCredentials = () => {
+    Axios.post("http://localhost:3000/add_credentials", {
+      title: title,
+      password: password,
+    });
+  };
+
   return (
-    <div className="App"></div>
+    <div className="App">
+      <div className="CredentialCreation">
+        <input
+          type="text"
+          placeholder="Ex. Facebook"
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Ex. password123"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <button onClick={addCredentials}>Add Credentials</button>
+      </div>
+    </div>
   );
 }
 
